e0190a11e8a7ccc8862bed0b6b8d72d6350fa1da
Jakub Sikora <jakub.sikora.en@gmail.com>
2015-06-03 23:48:56 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+
+    <title>Euro Nile Cargo</title>
+
+    <link href="css/bootstrap.css" rel="stylesheet" />
+	<link href="css/coming-sssoon.css" rel="stylesheet" />
+
+    <!--     Fonts     -->
+    <link href="http://netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Grand+Hotel' rel='stylesheet' type='text/css'>
+
+</head>
+
+<body>
+<nav class="navbar navbar-transparent navbar-fixed-top" role="navigation">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+      <ul class="nav navbar-nav">
+        <li>
+            <a href="/" >
+              <img src="/images/logo.png" style="height: 140px;">
+            </a>
+        </li>
+      </ul>
+
+      <ul class="nav navbar-nav navbar-right">
+             <li>
+                <a href="mailto:sales@euronile.com">
+                    <i class="fa fa-envelope-o"></i>
+                    Email
+                </a>
+            </li>
+       </ul>
+
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container -->
+</nav>
+<div class="main" style="background-image: url('images/cargo_bg.jpg')">
+
+<!--    Change the image source '/images/default.jpg' with your favourite image.     -->
+
+    <div class="cover black" data-color="black"></div>
+
+<!--   You can change the black color for the filter with those colors: blue, green, red, orange       -->
+
+    <div class="container">
+        <h1 class="logo cursive">
+            Coming Soon...
+        </h1>
+<!--  H1 can have 2 designs: "logo" and "logo cursive"           -->
+
+        <div class="content">
+            <h4 class="motto">We are working hard on the new look of the Euro Nile Cargo.</h4>
+            <div class="subscribe">
+                <h5 class="info-text">
+                    <i class="fa fa-phone-square fa-lg"> 0207 724 6111</i> &nbsp;&nbsp;&nbsp;&nbsp;
+                    <i class="fa fa-mobile fa-lg"> 077 33 70 74 74</i>
+                </h5>
+
+            </div>
+        </div>
+    </div>
+ </div>
+ </body>
+   <script src="js/jquery-1.10.2.js" type="text/javascript"></script>
+   <script src="js/bootstrap.min.js" type="text/javascript"></script>
+
+</html>
